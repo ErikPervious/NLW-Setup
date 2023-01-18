00e0dc0a6a3b7101846bbdc458c0650ae9ed4349
erikpervious <erickmatheus3000@gmail.com>
2023-01-17 21:01:15 -0300

    mobile added again

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,21 @@
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator 
+        color="#7C3AED"
+        size={30}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#09090A'
+  }
+});
